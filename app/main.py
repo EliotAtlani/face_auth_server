76c8c4ebd326ef90c4cd11de560e7ffd2cc24031
Atlani Eliot <eliot.atlani@student-cs.fr>
2024-07-18 17:16:25 +1000

    feat: Add websocket

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
 from datetime import datetime, timedelta
 import io
-from typing import List
 import numpy as np
-from fastapi import FastAPI, File, Form, HTTPException, UploadFile, BackgroundTasks
+from fastapi import (
+    FastAPI,
+    File,
+    Form,
+    UploadFile,
+    WebSocket,
+    WebSocketDisconnect,
+)
 from fastapi.responses import JSONResponse, StreamingResponse
 from imgbeddings import imgbeddings
 from PIL import Image
 from pinecone.grpc import PineconeGRPC as Pinecone
-from sqlalchemy.orm import Session
 from dotenv import load_dotenv
 from .helpers.crop_image import crop_image
 from .helpers.detect_faces import detect_faces
@@ -15,11 +20,14 @@ import os
 import jwt
 import uuid
 from fastapi.middleware.cors import CORSMiddleware
+import json
+import base64
 
+########################
+### Load Environment ###
+########################
 
-# Load environment variables from .env file
 load_dotenv()
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
@@ -27,12 +35,16 @@ ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 index = pc.Index("facial-recognition")
 
-# Initialize the rate limiter
 
+################
+### FastAPI ###
+################
 
 app = FastAPI()
 
-# CORS
+#######################
+### CORS Middleware ###
+#######################
 
 environment = os.getenv("ENVIRONMENT")
 
@@ -40,6 +52,7 @@ if environment == "development":
     origins = ["*"]  # Allow all origins in development
 else:
     origins = ["https://www.eliotatlani.fr"]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -50,6 +63,11 @@ app.add_middleware(
 )
 
 
+#################
+### Endpoints ###
+#################
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -58,6 +76,107 @@ async def root():
 @app.get("/ping/")
 async def ping():
     return {"message": "pong"}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await process_websocket_data(websocket, data)
+    except WebSocketDisconnect:
+        print("WebSocket connection closed")
+
+
+async def process_websocket_data(websocket: WebSocket, data: str):
+    try:
+        json_data = json.loads(data)
+        email = json_data.get("email")
+        image_data = json_data.get("imageData")
+
+        if not email or not image_data:
+            await websocket.send_text(
+                json.dumps({"success": False, "error": "Missing email or image data"})
+            )
+            return
+
+        # Decode base64 image
+        image_data = image_data.split(",")[
+            1
+        ]  # Remove the "data:image/jpeg;base64," part
+        image_bytes = base64.b64decode(image_data)
+        image = Image.open(io.BytesIO(image_bytes))
+
+        faces, img = detect_faces(image)
+
+        if len(faces) == 0:
+            await websocket.send_text(
+                json.dumps({"success": False, "error": "No faces found"})
+            )
+            return
+
+        cropped_face = crop_image(img, faces[0])
+
+        # Embedding
+        ibed = imgbeddings()
+        embedding = ibed.to_embeddings(cropped_face)
+
+        result = index.query(
+            namespace=email,
+            vector=embedding[0].tolist(),
+            top_k=5,
+            include_values=False,
+        )
+
+        if len(result["matches"]) == 0:
+            await websocket.send_text(
+                json.dumps(
+                    {"success": False, "error": "Face not authenticated", "stop": True}
+                )
+            )
+            return
+
+        if len(result["matches"]) < 5:
+            mean = np.mean(
+                [result["matches"][i]["score"] for i in range(len(result["matches"]))]
+            )
+        else:
+            mean = np.mean([result["matches"][i]["score"] for i in range(5)])
+
+        print("MEAN", mean)
+        if mean < 0.92:
+            await websocket.send_text(json.dumps({"error": "Face not authenticated"}))
+            return
+
+        # Create JWT token
+        access_token_expires = timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))
+        access_token = create_access_token(
+            data={"sub": email}, expires_delta=access_token_expires
+        )
+
+        add_new_images_to_index(email, embedding[0].tolist())
+
+        await websocket.send_text(
+            json.dumps(
+                {
+                    "success": True,
+                    "message": "Face authenticated successfully",
+                    "access_token": access_token,
+                }
+            )
+        )
+
+    except Exception as e:
+        print(f"Error processing WebSocket data: {str(e)}")
+        await websocket.send_text(
+            json.dumps(
+                {
+                    "success": False,
+                    "message": f"Error processing WebSocket data: {str(e)}",
+                }
+            )
+        )
 
 
 @app.post("/check-email/")
@@ -173,80 +292,76 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
 
 def add_new_images_to_index(email: str, embedding):
-
     uniqueId = str(uuid.uuid4())
     # Upsert all embeddings into Pinecone
     index.upsert(vectors=[{"id": uniqueId, "values": embedding}], namespace=email)
     print("Added new image to index")
 
 
-@app.post("/auth-face/")
-async def auth_face(
-    background_tasks: BackgroundTasks,
-    file: UploadFile = File(...),
-    email: str = Form(...),
-):
-    # Read the image file
-    contents = await file.read()
-    image = Image.open(io.BytesIO(contents))
+# @app.post("/auth-face/")
+# async def auth_face(
+#     background_tasks: BackgroundTasks,
+#     file: UploadFile = File(...),
+#     email: str = Form(...),
+# ):
+#     # Read the image file
+#     contents = await file.read()
+#     image = Image.open(io.BytesIO(contents))
 
-    # Save image
-    image.save("image.jpg")
+#     faces, img = detect_faces(image)
 
-    faces, img = detect_faces(image)
+#     if len(faces) == 0:
+#         print("No faces found")
+#         raise HTTPException(status_code=404, detail="No faces found")
 
-    if len(faces) == 0:
-        print("No faces found")
-        raise HTTPException(status_code=404, detail="No faces found")
+#     cropped_face = crop_image(img, faces[0])
 
-    cropped_face = crop_image(img, faces[0])
+#     # Embbeding
+#     ibed = imgbeddings()
 
-    # Embbeding
-    ibed = imgbeddings()
+#     embedding = ibed.to_embeddings(cropped_face)
 
-    embedding = ibed.to_embeddings(cropped_face)
+#     result = index.query(
+#         namespace=email,
+#         vector=embedding[0].tolist(),
+#         top_k=5,
+#         include_values=False,
+#     )
 
-    result = index.query(
-        namespace=email,
-        vector=embedding[0].tolist(),
-        top_k=5,
-        include_values=False,
-    )
+#     print(result)
 
-    print(result)
+#     if len(result["matches"]) == 0:
+#         print("Face not authenticated")
+#         return JSONResponse(
+#             content={"message": "Face not authenticated"}, status_code=403
+#         )
 
-    if len(result["matches"]) == 0:
-        print("Face not authenticated")
-        return JSONResponse(
-            content={"message": "Face not authenticated"}, status_code=403
-        )
+#     if len(result["matches"]) < 5:
+#         mean = np.mean(
+#             [result["matches"][i]["score"] for i in range(len(result["matches"]))]
+#         )
+#     else:
+#         mean = np.mean([result["matches"][i]["score"] for i in range(5)])
 
-    if len(result["matches"]) < 5:
-        mean = np.mean(
-            [result["matches"][i]["score"] for i in range(len(result["matches"]))]
-        )
-    else:
-        mean = np.mean([result["matches"][i]["score"] for i in range(5)])
+#     print("MEAN", mean)
+#     if mean < 0.92:
+#         print("Face not authenticated")
+#         return JSONResponse(
+#             content={"message": "Face not authenticated"}, status_code=401
+#         )
 
-    print("MEAN", mean)
-    if mean < 0.92:
-        print("Face not authenticated")
-        return JSONResponse(
-            content={"message": "Face not authenticated"}, status_code=401
-        )
+#     # Create JWT token
+#     access_token_expires = timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))
+#     access_token = create_access_token(
+#         data={"sub": email}, expires_delta=access_token_expires
+#     )
 
-    # Create JWT token
-    access_token_expires = timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))
-    access_token = create_access_token(
-        data={"sub": email}, expires_delta=access_token_expires
-    )
+#     background_tasks.add_task(add_new_images_to_index, email, embedding[0].tolist())
 
-    background_tasks.add_task(add_new_images_to_index, email, embedding[0].tolist())
-
-    return JSONResponse(
-        content={
-            "message": "Face authenticated successfully",
-            "access_token": access_token,
-        },
-        status_code=200,
-    )
+#     return JSONResponse(
+#         content={
+#             "message": "Face authenticated successfully",
+#             "access_token": access_token,
+#         },
+#         status_code=200,
+#     )
